04198e5cc81e9477cde830ce8936232b6706f1e6
BHushan <abhushanprathod@gmail.com>
2020-05-25 00:51:50 +0530

    added game-play status.

--- snakeladder.py
+++ snakeladder.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 MAX_VAL = 100
 DICE_FACE = 6
@@ -23,7 +22,7 @@ ladder_val = {
     40: 59,
     51: 67,
     63: 81,
-    71: 81
+    71: 91
 }
 
 
@@ -45,16 +44,21 @@ def ladder_up(current_value, old_value, player):
 
 def is_won(player, position):
     if position == MAX_VAL:
+        print("--------------")
+        print(" ＼(＾O＾)／ ")
         print("{} WON the game".format(player))
-        sys.exit(1)
+        print(" ※ \(^o^)/ ※ ")
+        print("--------------")
+        exit(1)
 
 
 def main_game(player, current_value, dice_value):
     old_value = current_value
     current_value = current_value + dice_value
-
+    print("{} : Moving from {} ".format(player, old_value))
     if current_value > MAX_VAL:
-        print("PASS")
+        print("{} need {} to win. CANT MOVE.".format(player, (MAX_VAL - old_value)))
+        return old_value
 
     if current_value in snakes_val:
         final_value = snakes_val.get(current_value)
@@ -67,6 +71,7 @@ def main_game(player, current_value, dice_value):
     else:
         final_value = current_value
 
+    print("{} : Moved to {}".format(player, final_value))
     return final_value
 
 
@@ -83,27 +88,21 @@ def main_start():
     player2_current_pos = 0
 
     while True:
+        input("{} press enter to play".format(player1))
+        dice_value = dice_role()
+        player1_current_pos = main_game(player1, player1_current_pos, dice_value)
+        is_won(player1, player1_current_pos)
+
+        input("{} press enter to play".format(player2))
+        dice_value = dice_role()
+        player2_current_pos = main_game(player2, player2_current_pos, dice_value)
+        is_won(player2, player2_current_pos)
+
         print("------------------")
         print("{} at position {}".format(player1, player1_current_pos))
         print("{} at position {}".format(player2, player2_current_pos))
         print("------------------")
-        val1 = input("{} your turn, press a key".format(player1))
-        print("{} Rolling Dice".format(player1))
-        dice_value = dice_role()
-        print("{} moving from {}".format(player1, player1_current_pos))
-        player1_current_pos = main_game(player1, player1_current_pos, dice_value)
-        print("{} moved to {}".format(player1, player1_current_pos))
-        is_won(player1, player1_current_pos)
-
-        print("------------------")
-        val2 = input("{} you turn, press a key ".format(player2))
-        print("{} Rolling Dice".format(player2))
-        dice_value = dice_role()
-        print("{} moving from {}".format(player2, player2_current_pos))
-        player2_current_pos = main_game(player2, player2_current_pos, dice_value)
-        print("{} moved to {}".format(player2, player2_current_pos))
-        is_won(player2, player2_current_pos)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main_start()
